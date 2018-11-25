61e3e84a63b6c1ecdaf10901a7f1704f41976fbb
avsh <avterus@gmail.com>
2018-11-25 13:27:39 +0300

    add token creation. add webhook processing.

--- begateway-payment/index.php
+++ begateway-payment/index.php
@@ -6,58 +6,22 @@ Author: eComCharge
 Version: 1.0.0
 */
 
+require_once dirname(__FILE__) . '/lib/BeGateway.php';
+
 new BeGatewayPayment;
 
 class BeGatewayPayment {
-  private static $pluginName = ''; // название плагина (соответствует названию папки)
-  private static $path = '';
-  private static $options = '';
+  const TEST_SHOP      = 361;
+  const TEST_KEY       = 'b8647b68898b084b836474ed8d61ffe117c9a01168d867f24953b776ddcb134d';
+  const TEST_DOMAIN    = 'checkout.begateway.com';
+  const PLUGIN_HASH    = 'b8d75e37be44b46d8c15f5d0fb6a8826'; // md5('begateway')
+
+  private static $pluginName = 'Оплата банковской картой онлайн'; // название плагина
+  private static $path = PLUGIN_DIR . '/begateway-payment'; //путь до файлов плагина.
 
   public function __construct() {
     mgActivateThisPlugin(__FILE__, array(__CLASS__, 'activate')); //Инициализация  метода выполняющегося при активации
-    mgAddShortcode('begateway-payment', array(__CLASS__, 'addPaymentForm'));
-
-    self::$pluginName = PM::getFolderPlugin(__FILE__);
-    self::$path = PLUGIN_DIR.self::$pluginName;
-
-    if(URL::isSection('order')){
-      if ( $_POST['payment'] ){
-        mgAddMeta('
-        <p style="display:none">
-        <input type="submit" id="begateway-submit" value="Оплатить" />
-        </p>
-        <script type="text/javascript">
-        $(document).ready( function(){
-          if($("input[name=phone]").val() == "" || $("input[name=email]").val() == ""){
-            return false;
-          }
-          $("#begateway-submit").click( function(){
-            $.ajax({
-              type: "POST",
-              async: false,
-              url: mgBaseDir+"/ajaxrequest",
-              dataType: \'json\',
-              data:{
-                mguniqueurl: "action/getPayLink",
-                pluginHandler: "begateway-payment",
-                paymentId: '.$_POST['payment'].',
-                mgBaseDir: mgBaseDir,
-              },
-              cache: false,
-              success: function(response){
-                if(response.data.status ==\'ok\'){
-                  window.location.href = response.data.result;
-                } else {
-                  errors.showErrorBlock(\'Oшибка получения токена платежа. Причина: \' + response.data.message);
-                }
-              }
-            });
-          })
-          setTimeout(function() {$( "#begateway-submit" ).trigger( "click" )}, 100);
-        })
-        </script>');
-      }
-    }
+    mgDeactivateThisPlugin(__FILE__, array(__CLASS__, 'deactivate'));
   }
 
   static function activate(){
@@ -65,18 +29,25 @@ class BeGatewayPayment {
     self::setDefultPluginOption();
   }
 
-  static function addPaymentForm() {
-    return "<div id='begateway-payment-container'></div>";
+  static function deactivate(){
+    USER::AccessOnly('1,4','exit()');
+    self::removePluginOption();
+  }
+
+  private static function removePluginOption(){
+    USER::AccessOnly('1,4','exit()');
+    $paymentId = self::getPaymentForPlugin();
+    if($paymentId){
+      $paymentId = self::removePaymentForPlugin();
+    }
   }
 
   private static function setDefultPluginOption(){
     USER::AccessOnly('1,4','exit()');
     $paymentId = self::getPaymentForPlugin();
 
-    if(MG::getSetting(self::$pluginName.'-option') == null || empty($paymentId)){
-      if(empty($paymentId)){
-        $paymentId = self::setPaymentForPlugin();
-      }
+    if(empty($paymentId)){
+      $paymentId = self::setPaymentForPlugin();
     }
   }
 
@@ -84,36 +55,222 @@ class BeGatewayPayment {
   * Возвращает идентификатор записи доставки из БД для плагина, по полю 'name'
   */
   static function getPaymentForPlugin(){
-    $result = array();
-    $dbRes = DB::query('
-    SELECT id
-    FROM `'.PREFIX.'payment`
-    WHERE `name` = '.DB::quote('BeGateway'));
 
-    if($result = DB::fetchAssoc($dbRes)){
+    if($result = self::getPluginData()){
       $sql = '
       UPDATE `'.PREFIX.'payment`
       SET `activity` = 1
-      WHERE `name` = '.DB::quote('BeGateway');
+      WHERE `add_security` = '.DB::quote(self::PLUGIN_HASH);
       DB::query($sql);
 
       return $result['id'];
     }
   }
 
+  static function getPluginData() {
+    $result = array();
+    $dbRes = DB::query('
+      SELECT *
+      FROM `'.PREFIX.'payment`
+      WHERE `add_security` = '.DB::quote(self::PLUGIN_HASH)
+    );
+
+    return DB::fetchAssoc($dbRes);
+  }
+
+  static function getPluginOptions() {
+    $result = array();
+    $decoded_params = array();
+
+    if ($result = self::getPluginData()) {
+      $params = json_decode($result['paramArray'], true);
+
+      if ($params) {
+        foreach ($params as $key => $value) {
+          $decoded_params []= CRYPT::mgDecrypt($value);
+        }
+      }
+    }
+
+    return $decoded_params;
+  }
+
+  static function getPluginId() {
+    if ($result = self::getPluginData()) {
+      return $result['id'];
+    }
+  }
+
+  static function getPaymentLastId() {
+    $result = array();
+
+    $dbRes = DB::query('
+      SELECT MAX(id) AS max_id
+      FROM `'.PREFIX.'payment`'
+    );
+
+    $result = DB::fetchAssoc($dbRes);
+    return $result['max_id'];
+  }
+
   static function setPaymentForPlugin(){
     USER::AccessOnly('1,4','exit()');
     $options = DB::quote(
-      '{"ID Магазина":"",' .
-      '"Секретный ключ":"",' .
-      '"Домен страницы оплаты":"",' .
-      '"Тестовый режим":""}'
+      '{"ID Магазина":"' . CRYPT::mgCrypt(self::TEST_SHOP) . '",' .
+      '"Секретный ключ":"' . CRYPT::mgCrypt(self::TEST_KEY) . '",' .
+      '"Домен страницы оплаты":"' . CRYPT::mgCrypt(self::TEST_DOMAIN) . '",' .
+      '"Включить оплату банковскими картами":"' . CRYPT::mgCrypt("1") . '",' .
+      '"Включить оплату картой Халва":"' . CRYPT::mgCrypt("0") . '",' .
+      '"Включить оплату через ЕРИП":"' . CRYPT::mgCrypt("0") . '",' .
+      '"Тестовый режим":"' . CRYPT::mgCrypt("true") . '"}'
     );
-    $sql = "INSERT INTO ".PREFIX."payment(name, activity, paramArray) VALUES
-    ('BeGateway', 1, {$options});";
+
+    $insert_id = self::getPaymentLastId() + 1;
+
+    $data = [
+      'id'           => $insert_id,
+      'name'         => DB::quote(self::$pluginName),
+      'activity'     => 1,
+      'paramArray'   => $options,
+      'urlArray'     => DB::quote(""),
+      'rate'         => 0,
+      'sort'         => $insert_id,
+      'add_security' => DB::quote(self::PLUGIN_HASH)
+    ];
+
+    $sql = "
+      INSERT INTO " . PREFIX . "payment(" . implode(',', array_keys($data)) . ") VALUES
+      (" . implode(',', array_values($data)) . ")";
 
     if(DB::query($sql)){
       return $thisId;
     }
+  }
+
+  static function removePaymentForPlugin(){
+    USER::AccessOnly('1,4','exit()');
+    $sql = '
+      DELETE FROM `' . PREFIX . 'payment`
+      WHERE `add_security` = ' . DB::quote(self::PLUGIN_HASH);
+
+    DB::query($sql);
+  }
+
+  static function getPaymentToken($order_id) {
+    $model = new Models_Order();
+    $order = $model->getOrder(" id = " . DB::quote($order_id, 1));
+
+    if (isset($order[$order_id]['delivery_cost']) && $order[$order_id]['delivery_cost'] > 0){
+      $summ = $order[$order_id]['summ'] + $order[$order_id]['delivery_cost'];
+    }else{
+      $summ = $order[$order_id]['summ'];
+    }
+
+    $currency = MG::getSetting('currencyShopIso');
+    if ($currency == "RUR") {
+      $currency = "RUB";
+    }
+
+    $transaction = new \BeGateway\GetPaymentToken;
+
+    $params = self::getPluginOptions();
+    \BeGateway\Settings::$shopId = $params[0];
+    \BeGateway\Settings::$shopKey = $params[1];
+    \BeGateway\Settings::$checkoutBase = 'https://' .$params[2];
+
+    $transaction->setTestMode($params[6] == 'true');
+
+    if (intval($params[3]) == 1) {
+      $transaction->addPaymentMethod(new \BeGateway\PaymentMethod\CreditCard);
+    }
+
+    if (intval($params[4]) == 1) {
+      $transaction->addPaymentMethod(new \BeGateway\PaymentMethod\CreditCardHalva);
+    }
+
+    if (intval($params[5]) == 1) {
+      $transaction->addPaymentMethod(new \BeGateway\PaymentMethod\Erip(
+        array(
+          'order_id' => $order_id,
+          'account_number' => $order[$order_id]['number']
+        )
+      ));
+    }
+
+    $successUrl      = SITE . "/payment?id=" . self::getPluginId() . "&pay=success&order_id={$order_id}";
+    $failUrl         = SITE . "/payment?id=" . self::getPluginId() . "&pay=fail&order_id={$order_id}";
+    $notificationUrl = SITE . "/payment?id=" . self::getPluginId() . "&pay=result&order_id={$order_id}";
+
+
+    $transaction->money->setAmount($summ);
+    $transaction->money->setCurrency($currency);
+    $transaction->setDescription("Оплата заказа # {$order[$order_id]['number']}");
+    $transaction->setTrackingId(implode('|', array($order_id, $order[$order_id]['number'])));
+    $transaction->setLanguage('ru');
+
+
+    $transaction->setNotificationUrl($notificationUrl);
+    $transaction->setSuccessUrl($successUrl);
+    $transaction->setDeclineUrl($failUrl);
+    $transaction->setFailUrl($failUrl);
+
+    try {
+      $response = $transaction->submit();
+
+      if ($response->isSuccess() ) {
+        return $response->getRedirectUrl();
+      } else {
+        throw new Exception($response->getMessage());
+      }
+    } catch (Exception $e) {
+      throw $e;
+    }
+  }
+
+  public static function processWebhook($order_id){
+    $model = new Models_Order();
+    $order = $model->getOrder(" id = " . DB::quote($order_id, 1));
+
+    if (empty($order)) {
+      throw new Exception("НЕКОРРЕКТНЫЕ ДАННЫЕ 1");
+    }
+
+    if ($order[$order_id]['id'] != $order_id) {
+      throw new Exception("НЕКОРРЕКТНЫЕ ДАННЫЕ 2");
+    }
+
+    $webhook = new \BeGateway\Webhook;
+    list($webhook_order_id, $webhook_order_number) = explode('|', $webhook->getTrackingId());
+
+    if ($order[$order_id]['id'] != $webhook_order_id || $order[$order_id]['number'] != $webhook_order_number) {
+      throw new Exception("НЕКОРРЕКТНЫЕ ДАННЫЕ 3");
+    }
+
+    $params = self::getPluginOptions();
+    \BeGateway\Settings::$shopId = $params[0];
+    \BeGateway\Settings::$shopKey = $params[1];
+
+    if (!$webhook->isAuthorized()) {
+      throw new Exception("НЕКОРРЕКТНЫЕ ДАННЫЕ 4");
+    }
+
+    $paymentAmount = new \BeGateway\Money;
+    $paymentAmount->setCents($webhook->getResponse()->transaction->amount);
+    $paymentAmount->setCurrency($webhook->getResponse()->transaction->currency);
+
+    $currency = MG::getSetting('currencyShopIso');
+    if ($currency == "RUR") {
+      $currency = "RUB";
+    }
+
+    $orderAmount = new \BeGateway\Money;
+    $orderAmount->setAmount($order[$order_id]['summ'] + $order[$order_id]['delivery_cost']);
+    $orderAmount->setCurrency($currency);
+
+    if ($paymentAmount->getCents() != $orderAmount->getCents()) {
+      throw new Exception("НЕКОРРЕКТНЫЕ ДАННЫЕ 5");
+    }
+
+    return $webhook->isSuccess();
   }
 }
